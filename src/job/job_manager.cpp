196e521b0dc6de26107f8a56da55c1555339ed1a
Clement Lemaire <lemairec@macbook-pro-de-clement.home>
2026-01-17 15:57:29 +0100

    Job

--- src/job/job_manager.cpp
+++ src/job/job_manager.cpp
@@ -8,39 +8,41 @@ JobManager::JobManager(){
 }
 
 void JobManager::init(){
-    std::string dir = DirectoryManager::instance().getHome()+"/lemca_data/job";
-    std::string s2 = "mkdir -p "+ dir + ";";
-    INFO(s2);
-    if(system( s2.c_str() )){
-        std::cerr << "*** can not execute : " << s2 << std::endl;;
-    };
-    
-    
-    time_t     now = time(0);
-    struct tm  tstruct;
-    char       buf[80];
-    tstruct = *localtime(&now);
-    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-    // for more information about date/time format
-    strftime(buf, sizeof(buf), "%Y_%m_%d_%H_%M_%S", &tstruct);
-    
-    m_begin = buf;
-    
-    dir = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin;
-    s2 = "mkdir -p "+ dir + ";";
-    INFO(s2);
-    if(system( s2.c_str() )){
-        std::cerr << "*** can not execute : " << s2 << std::endl;;
-    };
-    
-    m_file = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/job.txt";
-    handle60s();
-    
-    m_log_path = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/log.txt";
-    m_log_file.open(m_log_path);
-    
-    m_data_path = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/soil.txt";
-    m_data_file.open(m_data_path);
+    if(m_begin.empty()){
+        std::string dir = DirectoryManager::instance().getHome()+"/lemca_data/job";
+        std::string s2 = "mkdir -p "+ dir + ";";
+        INFO(s2);
+        if(system( s2.c_str() )){
+            std::cerr << "*** can not execute : " << s2 << std::endl;;
+        };
+        
+        
+        time_t     now = time(0);
+        struct tm  tstruct;
+        char       buf[80];
+        tstruct = *localtime(&now);
+        // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+        // for more information about date/time format
+        strftime(buf, sizeof(buf), "%Y_%m_%d_%H_%M_%S", &tstruct);
+        
+        m_begin = buf;
+        
+        dir = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin;
+        s2 = "mkdir -p "+ dir + ";";
+        INFO(s2);
+        if(system( s2.c_str() )){
+            std::cerr << "*** can not execute : " << s2 << std::endl;;
+        };
+        
+        m_file = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/job.txt";
+        handle60s();
+        
+        m_log_path = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/log.txt";
+        m_log_file.open(m_log_path);
+        
+        m_data_path = DirectoryManager::instance().getHome()+"/lemca_data/job/"+m_begin+"/soil.txt";
+        m_data_file.open(m_data_path);
+    }
 }
 
 int m_s = 0;
